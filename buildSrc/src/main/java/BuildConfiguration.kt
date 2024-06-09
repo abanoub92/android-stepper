84d8d9f66b006e41a06de87d3b36cfa2329f9b0b
abanoub92 <abanoub.eskander15@gmail.com>
2024-06-09 13:18:58 +0300

    Change check icon direction & upgrade version code and name

--- buildSrc/src/main/java/BuildConfiguration.kt
+++ buildSrc/src/main/java/BuildConfiguration.kt
@@ -5,8 +5,8 @@ object BuildConfiguration {
     const val compileSdkVersion = 29
     const val minSdkVersion = 21
 
-    const val versionCode = 1
-    const val versionName = "0.1.0"
+    const val versionCode = 4
+    const val versionName = "0.4.0"
 
     const val applicationId = "com.aceinteract.android.stepper"
 }
